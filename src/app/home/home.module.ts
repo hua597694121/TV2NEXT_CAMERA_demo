7213a78cc976b6448855ca436863d4b9dab29ddf
huaxiaolong <jack.hua@gosomo.cn>
2021-01-18 20:08:53 +0800

    Thin Code: reserve the home component.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,16 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
-import { MessageComponentModule } from '../message/message.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MessageComponentModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
